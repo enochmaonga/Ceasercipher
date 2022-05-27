ed2317f3ca606cb1d52306e03ebd70740c945ef0
enochmaonga <enoch.mokua@student.moringaschool.com>
2022-05-27 13:17:58 +0300

    test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 public class test {
 
     @Test
-        public void run_CeacerCipher_text_ArrayList() {
+        public void run_CeaserCipher_text_ArrayList() {
         System.out.println("Enter a plainText");
 
     }
